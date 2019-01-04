64fd181ced38fc8d732b5b31addf91ecb2e12e3e
Atul <atulanand94@gmail.com>
2019-01-04 21:07:51 +0100

    Basic two WhatsApp working. Need to unregister service workers on refresh though

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+console.log("Yolo");
+var webview = document.getElementById("whatsapp1");
+webview.addEventListener("dom-ready", function() {
+  webview.openDevTools();
+});
+var webview2 = document.getElementById("whatsapp2");
+webview2.addEventListener("dom-ready", function() {
+  webview2.openDevTools();
+});
